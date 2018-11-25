d8dbd54ac84dc9a7c1408f6ca48f41ecf0b2f652
sss140 <45291211+sss140@users.noreply.github.com>
2018-11-25 13:31:13 +0900

    投げる挙動を修正。それなりにそれっぽくなった。

--- ThrowableBalls/ThrowableBalls/ViewController.swift
+++ ThrowableBalls/ThrowableBalls/ViewController.swift
@@ -2,6 +2,7 @@
 //
 
 import UIKit
+import AudioToolbox
 class MakeBall{
     let gravity:CGFloat = 0.1//重力加速度
     let adjustRate:CGFloat = 0.05//調整のための係数
@@ -9,11 +10,11 @@ class MakeBall{
     let radius:CGFloat = 60.0//ボールの半径
     
     var velX:CGFloat = 0.0, velY:CGFloat = 0.0//ボールの速度
+    var culX:CGFloat = 0.0, culY:CGFloat = 0.0//ボールの計算用変数
     var posX:CGFloat = 0.0, posY:CGFloat = 0.0//ボールの位置
-    var accX:CGFloat = 0.0, accY:CGFloat = 0.0
     var floorX:CGFloat = 0.0, floorY:CGFloat = 0.0//画面の大きさ
     
-    var isDragged:Bool = false//ドラッグしてるかどうか
+    var isDragged:Bool = false//ドラッグしてるかどうかのフラグ
     
     var parentView = UIView()
     var circleView = UIImageView()//ボールのビュー
@@ -54,20 +55,30 @@ class MakeBall{
     @objc func longPressed(_ sender:UILongPressGestureRecognizer){
         switch sender.state {
         case .began:
-            parentView.bringSubviewToFront(circleView)
+            parentView.bringSubviewToFront(circleView)//タップしたボールを最前面に表示でする。
+            culX = sender.location(in: self.parentView).x
+            culY = sender.location(in: self.parentView).y
             isDragged = true
         break
         case .ended:
-            velX = (sender.location(in: self.parentView).x - velX) * 0.5
-            velY = (sender.location(in: self.parentView).y - velY) * 0.5
+            
+            posX = sender.location(in: self.parentView).x
+            posY = sender.location(in: self.parentView).y
+            velX = sender.location(in: self.parentView).x - culX
+            if abs(velX)>15.0{
+                velX = 15.0 * (abs(velX)/velX)
+            }
+            velY = sender.location(in: self.parentView).y - culY
+            if abs(velY)>15.0{
+                velY = 15.0 * (abs(velY)/velY)
+            }
             isDragged = false
         break
         default:
+            //ドラッグに合わせてボールを表示
             circleView.center = sender.location(in: self.parentView)
-            posX = sender.location(in: self.parentView).x
-            velX = posX
-            posY = sender.location(in: self.parentView).y
-            velY = posY
+            culX += (sender.location(in: self.parentView).x - culX) * 0.9
+            culY += (sender.location(in: self.parentView).y - culY) * 0.9
         break
         }
     }
@@ -79,26 +90,30 @@ class MakeBall{
         //ボールが床に着地したら跳ね返る
         if(posY > floorY ){
             posY = floorY-(posY - floorY)
-            velY = -1 * velY * bounceRate
+            velY = collide(targetVel: velY)
         }
-            if(posY < radius ){
-                posY = radius - (posY - radius)
-                velY = -1 * velY * bounceRate
-            }
-            
         circleView.center.y = posY
+        
         //壁にあたったら跳ね返る
         posX += velX
         if(posX>floorX){
             posX = floorX - (posX - floorX)
-            velX = -1 * velX * bounceRate
+            velX = collide(targetVel: velX)
         }
         if(posX<radius){
             posX = radius - (posX - radius)
-            velX = -1 * velX * bounceRate
+            velX = collide(targetVel: velX)
         }
         circleView.center.x = posX
         }
+    }
+    func collide(targetVel:CGFloat)->CGFloat{
+        let vel:CGFloat = targetVel * -1 * bounceRate
+        /*if abs(vel) > 0.8{
+        AudioServicesPlaySystemSound(1003)
+        AudioServicesDisposeSystemSoundID(1003)
+        }*/
+        return vel
     }
 }
 
